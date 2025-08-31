0ff4b40e68872cbdc4955c6c936230ed766d8f1a
shimaaNasserr <shimaanasser304@gmail.com>
2025-08-31 16:49:40 +0300

    change chatbot file name

--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../../components/NavBar/Navbar'
-import ChatbotWidget from '../ChatbotWidget./ChatbotWidget'
+import ChatbotWidget from '../ChatbotWidget/ChatbotWidget'
 import { Footer } from '../../components/Footer/Footer'
 
 export const Layout = () => {
